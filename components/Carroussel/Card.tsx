b1dca03e408cfeeaed3e1608857b4d902ea15d3b
JIdayyy <julien_abbadie@hotmail.fr>
2021-07-04 13:33:05 +0200

    push

--- components/Carroussel/Card.tsx
+++ components/Carroussel/Card.tsx
@@ -26,24 +26,26 @@ export default function Card({ index, track, setIsClicked }) {
       key={index}
       className=" mx-4 border-2  hover:scale-105 h-full flex flex-col  w-96 relative "
     >
-      <span className=" z-40 text-white font-bold p-7 text-2xl">
-        {track.title}
-      </span>
-      <span className=" z-40 text-white font-bold p-7 text-2xl">
-        {track.artist.name}
-      </span>
-      <span className=" w-full h-full hover:block hidden z-40 text-white font-bold p-7 text-2xl">
-        {track.duration}
-      </span>
-
-      <button
-        onClick={handleClick}
-        className="absolute z-50 bottom-0 text-white font-bold p-7 text-2xl"
-      >
-        <div className="border-4 w-full items-end justify-end active:bg-gray-600 active:scale-90 rounded-full p-3">
-          <Image width={20} height={25} src={"/controls/play.png"} />
-        </div>
-      </button>
+      <div className="w-full flex flex-col bg-black bg-opacity-20 h-full">
+        {" "}
+        <span className=" z-40 text-white font-bold p-7 text-2xl">
+          {track.title}
+        </span>
+        <span className=" z-40 text-white font-bold p-7 text-2xl">
+          {track.artist.name}
+        </span>
+        <span className=" w-full h-full hover:block hidden z-40 text-white font-bold p-7 text-2xl">
+          {track.duration}
+        </span>
+        <button
+          onClick={handleClick}
+          className="absolute  items-end justify-end flex w-full z-50 bottom-0 text-white font-bold p-7 text-2xl"
+        >
+          <div className="border-4  items-end justify-end active:bg-gray-600 active:scale-90 rounded-full p-3">
+            <Image width={30} height={25} src={"/controls/play.png"} />
+          </div>
+        </button>
+      </div>
     </div>
   );
 }
